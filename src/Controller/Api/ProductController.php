4d203ae214c4478bcb45acc2f92794656709329f
SernaR <rserna@outlook.fr>
2020-12-01 16:36:48 +0100

    store method in Product

--- src/Controller/Api/ProductController.php
+++ src/Controller/Api/ProductController.php
@@ -2,20 +2,27 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\User;
 use App\Entity\Product;
+use App\Repository\UserRepository;
 use App\Repository\ProductRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\Exception\NotEncodableValueException;
 
 /**
- * @Route("/api/products", name="product")
+ * @Route("/api", name="product")
  */
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/{productId<[0-9]+>}", name="_find", methods={"GET"})
+     * @Route("/products/{productId<[0-9]+>}", name="_find", methods="GET")
      */
-    public function find(ProductRepository $repo, $productId) {
+    public function find(ProductRepository $repo, $productId) { 
         $product =  $repo->find($productId);
         
         if (!$product) {
@@ -29,12 +36,43 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @Route("/", name="_index", methods={"GET"})
+     * @Route("/products", name="_index", methods="GET")
      */
     public function index(ProductRepository $repo)
     {
         $products = $repo->findBy([], ['createdAt' => 'DESC']);
         return $this->json( $products, 200, [], ['groups' => "products"]);
     }
+
+    /**
+     * @Route("/products/{user}", name="_store", methods="POST")
+     */
+    public function store(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, User $user, ValidatorInterface $validator)
+    {
+        $json = $request->getContent();
+        try {
+            $product = $serializer->deserialize($json, Product::class, 'json');
+            $product->setOwner($user); //jwt
+            
+            $errors = $validator->validate($product);
+            if(count($errors) > 0) {
+                return $this->json($errors, 400); 
+            }
+            
+            $em->persist($product);
+            $em->flush();
+
+            return $this->json($product, 201, [], ['groups' => "products"]);
+
+        } catch (NotEncodableValueException $e) {
+            return $this->json([
+                'status' => 400,
+                'message' => $e->getMessage()
+            ], 400);
+        }
+    }
+
+    //remove
+    //edit
 
 }
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -7,7 +7,9 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\ProductRepository;
+
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ProductRepository::class)
@@ -28,6 +30,7 @@ class Product
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups("products")
+     * @Assert\NotBlank(message="Le nom du produit est obligatoire")
      */
     private $name;
 
